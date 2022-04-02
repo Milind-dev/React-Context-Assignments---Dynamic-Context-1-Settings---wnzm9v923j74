d93e76ee755863f3365658e320129ab08444c5f9
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-04-02 23:03:16 +0530

    React (Context) Assignments - Dynamic Context (1) Settings

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,16 +1,14 @@
 import React, { createContext } from "react";
 
-
-const UserContext = createContext()
+const UserContext = createContext();
 
 const Wrapper = (props) => {
+  const greeting = "Hello";
 
-    const greeting = "Hello"
-  
-    return (
-        <UserContext.Provider value={{ greeting }}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
-export { Wrapper, UserContext }
+  return (
+    <UserContext.Provider value={{ greeting }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+export { Wrapper, UserContext };
